0dfbd2ebfb4106f06f4c815409757afdb6f37049
Ewan <ewanrobsonn@gmail.com>
2023-12-13 23:46:24 +1100

    renamed pallet colors

--- lua/ignite/init.lua
+++ lua/ignite/init.lua
@@ -1,10 +1,36 @@
-local ignite = {}
+local M = {}
 
-function ignite.setup(overrides)
-  ignite.__overrides = overrides or {}
+function M.setup(setup)
+  M.__overrides = setup.overrides or nil
 end
 
-function ignite.load()
+function M.get_tbl(tbl, name)
+  if M.__overrides and M.__overrides[name] then
+    tbl = vim.tbl_deep_extend("force", tbl, M.__overrides[name])
+  end
+  return tbl
+end
+
+function M.set_terminal_colors(pallet)
+  vim.g.terminal_color_0  = pallet.background1
+  vim.g.terminal_color_8  = pallet.grey
+  vim.g.terminal_color_1  = pallet.red
+  vim.g.terminal_color_9  = pallet.red
+  vim.g.terminal_color_2  = pallet.dark_green
+  vim.g.terminal_color_10 = pallet.orange
+  vim.g.terminal_color_3  = pallet.yellow
+  vim.g.terminal_color_11 = pallet.yellow
+  vim.g.terminal_color_4  = pallet.blue
+  vim.g.terminal_color_12 = pallet.blue
+  vim.g.terminal_color_5  = pallet.pink
+  vim.g.terminal_color_13 = pallet.pink
+  vim.g.terminal_color_6  = pallet.light_blue
+  vim.g.terminal_color_14 = pallet.light_blue
+  vim.g.terminal_color_7  = pallet.light_grey
+  vim.g.terminal_color_15 = pallet.very_light_grey
+end
+
+function M.load()
   if vim.version().minor < 8 then
     vim.notify_once("ignite.nvim: requires neovim version 0.8 or higher", vim.log.levels.ERROR)
     return
@@ -17,10 +43,14 @@ function ignite.load()
   vim.g.colors_name = "ignite"
   vim.o.termguicolors = true
 
-  local groups = require("ignite.groups").get()
+  local pallet = M.get_tbl(require("ignite.pallet"), "pallet")
+  local groups = M.get_tbl(require("ignite.groups").get(pallet), "groups")
+
   for group, opts in pairs(groups) do
     vim.api.nvim_set_hl(0, group, opts)
   end
+
+  M.set_terminal_colors(pallet)
 end
 
-return ignite
+return M
--- lua/ignite/lualine.lua
+++ lua/ignite/lualine.lua
@@ -1,36 +1,34 @@
 local pallet = require("ignite.pallet")
 
-local theme = {
+return {
   normal = {
-    a = { bg = pallet.light_blue, fg = pallet.black, gui = "bold" },
+    a = { bg = pallet.operator, fg = pallet.cursor_under_txt, gui = "bold" },
     b = { bg = pallet.background3 },
     c = { bg = pallet.background2 },
   },
   insert = {
-    a = { bg = pallet.dark_green, fg = pallet.black, gui = "bold" },
+    a = { bg = pallet.string, fg = pallet.cursor_under_txt, gui = "bold" },
     b = { bg = pallet.background3 },
     c = { bg = pallet.background2 },
   },
   visual = {
-    a = { bg = pallet.red, fg = pallet.black, gui = "bold" },
+    a = { bg = pallet.keyword, fg = pallet.cursor_under_txt, gui = "bold" },
     b = { bg = pallet.background3 },
     c = { bg = pallet.background2 },
   },
   replace = {
-    a = { bg = pallet.dark_green, fg = pallet.black, gui = "bold" },
+    a = { bg = pallet.string, fg = pallet.cursor_under_txt, gui = "bold" },
     b = { bg = pallet.background3 },
     c = { bg = pallet.background2 },
   },
   command = {
-    a = { bg = pallet.pink, fg = pallet.black, gui = "bold" },
+    a = { bg = pallet.constant, fg = pallet.cursor_under_txt, gui = "bold" },
     b = { bg = pallet.background3 },
     c = { bg = pallet.background2 },
   },
   inactive = {
-    a = { bg = pallet.background3, fg = pallet.dark_grey, gui = "bold" },
+    a = { bg = pallet.background3, fg = pallet.comment, gui = "bold" },
     b = { bg = pallet.background3 },
     c = { bg = pallet.background2 },
   },
 }
-
-return theme
